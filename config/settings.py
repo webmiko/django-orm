91bee81f5d8c1508d24aa461930ae656e4bf747d
webmiko <webmiko@icloud.com>
2026-03-03 00:47:11 +0300

    feat(readme, tests, static): обновление документации, добавление тестов и статических файлов
    - Обновлен README.md с информацией о шаблонах и статике для магазина.
    - Добавлены тесты для представлений, моделей и URL в catalog/tests.py, включая проверки на 200 и 404 статусы, а также валидацию данных.
    - Добавлены новые статические файлы: Bootstrap_logo.svg.png и not-image.png.

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
